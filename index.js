a03b45d42984783ccfb936ed78b9418811e8a346
Jacob <jacobmunden@gmail.com>
2022-07-14 21:41:30 -0600

    commit tree 0b8f01defca835e92e6ac4c1305e560231a42ff8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const matchingDrivers = drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase())
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+    const fuzzyLetters = drivers.filter(drivers => drivers.toLowerCase().startsWith(string.toLowerCase()))
+    console.log(fuzzyLetters);
+    return fuzzyLetters;
+}
+
+function matchName(driver, string) {
+    const matches = driver.filter(driver => driver.name.toLowerCase() === string.toLowerCase())
+    return matches;
+}
